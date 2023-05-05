72d932d039582b50ecce6559e7dda541f96d7169
Eslam Mohamed <91751277+eslammohamedtolba@users.noreply.github.com>
2023-05-05 15:02:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
 #include "Trees.h"
 using namespace std;
 int main() {
-    int Nstudents;
-    cout<<"please enter the number of students ->>>"<<endl;
-    cin>>Nstudents;
-    vector<Node*>Students(Nstudents);
-    for(int i=0;i<Nstudents;i++){
-        Students[i]=new Node();
-        cin>>Students[i]->Id;cin.ignore();
-        getline(cin,Students[i]->Name);
-        cin>>Students[i]->Gpa>>Students[i]->Department;
-    }
     //Interface
 }
